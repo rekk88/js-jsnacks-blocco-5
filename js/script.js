848693cdcea0f66844b531c6c7b14b057e555fa0
maicol <maicol.matteoli@gmail.com>
2021-06-15 17:41:15 +0200

    pulizia codice es 4

--- js/script.js
+++ js/script.js
@@ -58,16 +58,19 @@
 
 
 // --------------------------------------------es3--------------------------------------------------------
+//da non fare quando si lavora con gli oggetti 
+//    zucchina.varietà = prompt("inserisci varietà : ");
+//    zucchina.peso = rand(0,10);
+//    zucchina.lunghezza = rand(10,30);
+//     orto.push(zucchina);
+
+
 
 var z15 = []; //zucchine con lunghezza >= 15
 var peso15 = 0 ;
 var z14 = []; //zucchine con lunghezza < 15
 var peso14 = 0 ;
-// var orto = [];
-var orto = new Array();
-// Array.prototype.push = function(){ alert('non pushi piuu')};
-var test = new Object();
-console.log(test);
+
 var zucchina = {
     varietà : " ",
     peso : 0 ,
@@ -77,15 +80,12 @@ var zucchina = {
 
 //carico 10 zucchine nel vettore orto
 for(var i=0 ; i < 10 ; i++){
-//    zucchina.varietà = prompt("inserisci varietà : ");
-//    zucchina.peso = rand(0,10);
-//    zucchina.lunghezza = rand(10,30);
 
     orto.push({varietà : prompt("inserisci : "), peso : rand(0,10) , lunghezza : rand(10,30) });
-//     orto.push(zucchina);
-//    console.log("lunghezza zucchina : " , zucchina.lunghezza);
 }
 console.log(orto);
+
+//caricamento vettori z14 z15
 for(var i=0 ; i < orto.length ; i++){
     if(orto[i].lunghezza >= 15){
         z15.push(orto[i]);
@@ -96,6 +96,8 @@ for(var i=0 ; i < orto.length ; i++){
         console.log(z14);
     }
 }
+
+//calcolo peso totale dei due gruppi di zucchine 
 for(var i=0 ; i < z15.length ; i++){
     peso15 += z15[i].peso;
 }
@@ -104,10 +106,14 @@ for(var i=0 ; i < z14.length ; i++){
     peso14 += z14[i].peso;
 }
 
+
+//output
 console.log("peso 15 : ", peso15);
 console.log("peso 14 : ", peso14);
 
 
+
+//generazione numero random 
 function rand(min , max){
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
